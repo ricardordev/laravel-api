fa2b9a5ef86c483f86d6a167c48eff6c886b3957
Ricardo Albrecht <ricardoalbrecht1@gmail.com>
2026-05-11 13:33:49 -0300

    first commit as example

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,5 @@ Route::get('/', function () {
     return response()->json([
         'message' => 'There is nothing here.',
         'status' => 'API Active',
-        'documentation' => 'http://api.laravel.local:8000/api/docs'
     ]);
 });
