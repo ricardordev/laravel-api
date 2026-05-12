bb233cdb2407835754f117d722c787d0131ee1c9
Ricardo Albrecht <ricardoalbrecht1@gmail.com>
2026-05-12 00:14:59 -0300

    fixing details

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->json([
         'message' => 'There is nothing here.',
-        'status' => 'API Active',
+        'status' => true,
+        'name' => env('APP_NAME'),
+        'url' => env('APP_URL'),
+        'rate_limit' => (int) env('API_RATE_LIMIT'),
     ]);
 });
